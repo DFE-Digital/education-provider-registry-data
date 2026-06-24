64ef99bc343e5284654e5eff6a8a89b0d5397478
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-06-24 14:45:03 +0100

    update models for scaffold

---
+++ generator/src/DfE.EducationProviderRegistry.Data.DatabaseModels/Models/GroupType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+public partial class GroupType
+{
+    public long GroupTypeId { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<GroupRecord> GroupRecord { get; set; } = new List<GroupRecord>();
+}
---
+++ generator/src/DfE.EducationProviderRegistry.Data.DatabaseModels/Models/Title.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DfE.EducationProviderRegistry.Data.DatabaseModels.Models;
+
+public partial class Title
+{
+    public long TitleId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Person> Person { get; set; } = new List<Person>();
+}
